eaeb1076f81ecaf30594cefffe440d0ef968fe81
Andry-QA <whyucry25@gmail.com>
2021-06-17 16:03:53 +0300

    Changing files to catch exception
    lesson 4.2 step 6

--- venv/pages/base_page.py
+++ venv/pages/base_page.py
@@ -1,9 +1,21 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
     # Конструктор
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     # Метлод открывает нужную страницу в браузере
     def open(self):
         self.browser.get(self.url)
+
+    # Метод перехвата исключения существования элемента
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
