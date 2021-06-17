eb3ee89497aa08a066735edde0685670f9f18d85
Andry-QA <whyucry25@gmail.com>
2021-06-17 15:49:46 +0300

    Changing files according to 5th step

--- README.md
+++
@@ -1,2 +0,0 @@
-# Finial_Projct_Selenium
- This is repository for complieting finial task of course "Stepik: Selenium autotests by python"
--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 from pages.main_page import MainPage
+
+
 # pytest -v --tb=line --language=en test_main_page.py
-
-
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
@@ -13,3 +13,9 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                     # открываем страницу
     page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
 
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
