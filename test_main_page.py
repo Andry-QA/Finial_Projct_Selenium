dde57584a4e799856985fa5471884a57d64220ff
Andry-QA <whyucry25@gmail.com>
2021-06-17 16:31:46 +0300

    Adding locatorts.py
    Also configurating other files according to new locators

--- test_main_page.py
+++ test_main_page.py
@@ -1,21 +1,16 @@
 from pages.main_page import MainPage
+link1 = "http://selenium1py.pythonanywhere.com/"                                                        # MainPageLink
+link2 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"  # 209 book link
 
 
 # pytest -v --tb=line --language=en test_main_page.py
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
-
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link1)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                     # открываем страницу
     page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, link1)
     page.open()
     page.should_be_login_link()
